ab09ccde42cc6ab3e70f62f7bf4145cf23eb8970
joaorcosme <joaorcosme@gmail.com>
2018-08-08 03:01:14 +0000

    Adding comments to force_driver script

--- can/force_driver.sh
+++ can/force_driver.sh
@@ -17,3 +17,10 @@ sudo ./softing_canprousb restart
 sleep 1
 cd /etc/softing
 sudo ./mk_devices 15
+
+# If this does not solve the issue, try the following:
+#  1) Plug USB to the port
+#  2) run 'sudo ./softing_canprousb restart'
+#  3) Remove USB
+#  4) Plug USB back to the port
+#  5) Should work now...
