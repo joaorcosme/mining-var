b48ebdd1b27ee80c9918c2a80e2acb0e9c96d01a
joaorcosme <joaorcosme@gmail.com>
2018-08-13 22:50:54 -0300

    Extracting GUI code to DetectionGUI class

--- can/CANTest.cpp
+++ can/CANTest.cpp
@@ -2,11 +2,7 @@
 #include "CANL2.h"
 #include "CANUtils.h"
 #include "CANproChannel.h"
-
-#include <nana/gui.hpp>
-#include <nana/gui/widgets/button.hpp>
-#include <nana/gui/widgets/label.hpp>
-#include <nana/gui/widgets/listbox.hpp>
+#include "DetectionGUI.h"
 
 #include <cerrno>
 #include <cstring>
@@ -88,6 +84,11 @@ static void interruption(CAN_HANDLE channel, backsense::RadarStateDB &stateDB,
                 if (DEBUG_RECV_DATA) {
                     printDetectionData(*state);
                 }
+
+                // This is probably the most important step in this loop:
+                // we've read the raw data from the CAN bus, converted into
+                // a DetectionData object, and now we are able to update the DB,
+                // overwriting the state for the corresponding object id.
                 stateDB.updateState(std::move(*state));
             }
         }
@@ -105,89 +106,19 @@ int main(int argc, char **argv)
 
     try {
         can::CANproChannel channel;
-        can::backsense::RadarStateDB stateDB(1 /* nSensors*/);
+        can::backsense::RadarStateDB stateDB(1);
         std::promise<void> exitSignal;
         std::future<void> futureSignal = exitSignal.get_future();
         std::thread interruptionHandler(can::interruption, channel.getHandle(),
                                         std::ref(stateDB),
                                         std::move(futureSignal));
-        ///// experiment begin
-        nana::form fm(nana::rectangle{100, 100, 800, 400});
 
-        nana::label lab{fm, "Label"};
-        lab.format(true);
-
-        nana::label label{fm, nana::rectangle(25, 10, 100, 20)};
-        label.caption("Detection Table");
-
-        nana::button btn{fm, nana::rectangle{370, 350, 60, 30}};
-        btn.caption("Quit");
-        btn.events().click([&fm] { fm.close(); });
-
-        auto cell_translator = [](
-            const std::experimental::optional<can::backsense::DetectionData>
-                &data) {
-
-            constexpr unsigned numParams = 5;
-            std::vector<nana::listbox::cell> cells;
-
-            if (data) {
-                cells.emplace_back(data->getStrHexId());
-                cells.emplace_back(std::to_string(data->getPolarRadius()));
-                cells.emplace_back(std::to_string(data->getPolarAngle()));
-                cells.emplace_back(std::to_string(data->getX()));
-                cells.emplace_back(std::to_string(data->getY()));
-                cells.emplace_back(std::to_string(data->getRelativeSpeed()));
-                cells.emplace_back(std::to_string(data->getSignalPower()));
-                cells.emplace_back(std::to_string(data->getObjectId()));
-                cells.emplace_back(
-                    std::to_string(data->getObjectAppearanceStatus()));
-                cells.emplace_back(std::to_string(data->getTriggerEvent()));
-                cells.emplace_back(std::to_string(data->getDetectionFlag()));
-            } else {
-                for (int i = 0; i < numParams; ++i) {
-                    cells.emplace_back("");
-                }
-            }
-            return cells;
-        };
-
-        nana::listbox lsbox{fm, nana::rectangle{25, 40, 750, 250}};
-
-        lsbox.append_header("Id");
-        lsbox.append_header("Radius");
-        lsbox.append_header("Angle");
-        lsbox.append_header("X Coord");
-        lsbox.append_header("Y Coord");
-        lsbox.append_header("Relative Speed");
-        lsbox.append_header("Signal Power");
-        lsbox.append_header("Object Id");
-        lsbox.append_header("Appearance Status");
-        lsbox.append_header("Trigger Event");
-        lsbox.append_header("Detection Flag");
-
-        lsbox.at(0).shared_model<std::recursive_mutex>(stateDB.getSensorData(0),
-                                                       cell_translator);
-
-        fm.show();
-
-        std::thread updater([&lsbox]() {
-            using namespace std::chrono_literals;
-            while (true) {
-                std::this_thread::sleep_for(1s);
-                nana::API::refresh_window(lsbox);
-            }
-        });
-        updater.detach();
-
-        nana::exec();
-        ///// experiment end
-
-        std::getchar();
+        gui::DetectionGUI interface(stateDB);
+        interface.launchGUI(); // blocking call
 
         exitSignal.set_value();
-        can::CANUtils::resetChip(channel.getHandle());
 
+        can::CANUtils::resetChip(channel.getHandle());
         interruptionHandler.join();
 
     } catch (std::runtime_error &ex) {
