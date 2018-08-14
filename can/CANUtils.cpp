daabbb71b4a3643b3ca447ed5b4eff57c1102d5a
joaorcosme <joaorcosme@gmail.com>
2018-08-14 20:01:17 -0300

    Adding missing files for CANUtils

---
+++ can/CANUtils.cpp
@@ -0,0 +1,51 @@
+#include "CANUtils.h"
+
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+
+using can::CANUtils;
+
+constexpr std::array<char, 16> CANUtils::m_hexMap;
+
+int CANUtils::readBusEvent(CAN_HANDLE can, PARAM_STRUCT &retParam)
+{
+    retParam.DataLength = 3;
+    return CANL2_read_ac(can, &retParam);
+}
+
+std::string CANUtils::formatHexStr(const __u8 *data, const __s32 len)
+{
+    // input: an array of bytes, like: |100|045|099|000|253|022|009|150|
+    // each byte is a __u8 type, which corresponds to a char
+    //
+    // output: a string where each byte is represented in its hexadecimal
+    // representation
+
+    constexpr unsigned MAXLEN = 8;
+    assert(len <= MAXLEN);
+
+    // each byte will take 3 positions in the string, like:
+    // "64 2D 63 00 FD 16 09 96"
+    std::string hexData(3 * MAXLEN, ' ');
+    for (unsigned i = 0; i < len; ++i) {
+        hexData[3 * i] = m_hexMap[(data[i] & 0xF0) >> 4];
+        hexData[3 * i + 1] = m_hexMap[data[i] & 0x0F];
+    }
+    return hexData;
+}
+
+void CANUtils::printReceivedData(int frc, const PARAM_STRUCT &param)
+{
+    // for now, we are only interested in this type of frame
+    if (frc == CANL2_RA_DATAFRAME) {
+        std::cout << "RCV STD CAN1 :::: "
+                  << "ID " << std::hex << std::setfill(' ') << std::setw(5)
+                  << param.Ident << " :: LEN " << param.DataLength
+                  << " :: DATA "
+                  << formatHexStr(param.RCV_data, param.DataLength)
+                  << std::endl;
+    } else {
+        assert(false);
+    }
+}
---
+++ can/CANUtils.h
@@ -0,0 +1,34 @@
+#ifndef __CAN_UTILS_CHANNEL_H_
+#define __CAN_UTILS_CHANNEL_H_
+
+#include "CANL2.h"
+
+#include <array>
+#include <string>
+
+namespace can {
+
+class CANUtils
+{
+  public:
+    CANUtils &operator=(const CANUtils) = delete;
+    CANUtils(const CANUtils &) = delete;
+
+    CANUtils() = default;
+
+    static int readBusEvent(CAN_HANDLE can, PARAM_STRUCT &retParam);
+    static void resetChip(CAN_HANDLE can) { CANL2_reset_chip(can); }
+    static void printReceivedData(int frc, const PARAM_STRUCT &param);
+
+  private:
+    static std::string formatHexStr(const __u8 *data, const __s32 len);
+
+  private:
+    static constexpr std::array<char, 16> m_hexMap{'0', '1', '2', '3', '4', '5',
+                                                   '6', '7', '8', '9', 'a', 'b',
+                                                   'c', 'd', 'e', 'f'};
+};
+
+} // namespace can
+
+#endif // __CAN_UTILS_CHANNEL_H_
