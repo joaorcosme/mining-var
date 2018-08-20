6ebc369ab3dc949de44849727a7c38f394a7337e
joaorcosme <joaorcosme@gmail.com>
2018-08-19 21:44:50 -0300

    Drawing obstacules over scene

--- augreality/MainAR.cpp
+++ augreality/MainAR.cpp
@@ -18,9 +18,6 @@
  *
  */
 
-#include "BarGraph.h"
-#include "SensorSimulator.h"
-
 #include "../can/BSFrameHandler.h"
 #include "../can/CANUtils.h"
 #include "../can/CANproChannel.h"
@@ -30,20 +27,12 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 
-#include <chrono>
 #include <future>
 #include <iomanip>
 #include <stdexcept>
 #include <thread>
 #include <utility>
 #include <vector>
-
-template <typename T> static std::string buildDisplayTextValue(const T value)
-{
-    std::stringstream builder;
-    builder << std::setprecision(3) << value << "m";
-    return builder.str();
-}
 
 static void launchARWindowLoop(const can::backsense::RadarStateDB& stateDB)
 {
@@ -54,27 +43,75 @@ static void launchARWindowLoop(const can::backsense::RadarStateDB& stateDB)
         throw std::runtime_error("Can't open camera.");
     }
 
-    // use the sensor simulator for debbugging
-    using namespace std::chrono_literals;
-    augreality::SensorSimulator s1(2000ms);
+    cap >> frame;
 
-    augreality::BarGraph bGraph(cv::Point(50, 70), 60, 300);
+    // 30m ^  +--------------+
+    //     |  |              |
+    //     |  |              |
+    //     |  |              |
+    //     |  |              |     X ^
+    //     |  |              |       |
+    //     |  |              |       |
+    //     |  |      O       |       |
+    //     |  |      |       |       +------>
+    //     |  | O    |       |              Y
+    //     |  |  \   |   O   |
+    //     |  |   \  |  /    |
+    //     |  |    \ | /     |
+    //     |  |     \|/      |
+    //  0m v  +------S-------+
+    //
+    //        <-------------->
+    //      -5m             +5m
+
+    constexpr unsigned MAX_PHY_X = 10;
+    constexpr unsigned MAX_PHY_Y = +5;
+    constexpr unsigned MIN_PHY_Y = -5;
+    constexpr unsigned DISPLAY_SPACING_X = 20;
+
+    const unsigned sensorX = frame.rows - (DISPLAY_SPACING_X / 2);
+    const unsigned sensorY = frame.cols / 2;
+    const cv::Point sensorP(sensorY, sensorX);
+
+    const unsigned rectHeight = frame.rows - DISPLAY_SPACING_X;
+    const unsigned rectWidth = rectHeight;
+    const unsigned pxStepX = rectHeight / MAX_PHY_X;
+    const unsigned pxStepY = rectWidth / (MAX_PHY_Y - MIN_PHY_Y);
+
+    const unsigned rectX = DISPLAY_SPACING_X / 2;
+    const unsigned rectY = sensorY + (MIN_PHY_Y * pxStepY);
+    const cv::Point rectP1(rectY, rectX);
+    const cv::Point rectP2(rectY + rectWidth, rectX + rectHeight);
+    const cv::Scalar rectColor(0, 0, 255);
+
+    constexpr unsigned obstRadius = 10;
+    const cv::Scalar obstColor(0, 255, 255);
+
+    auto toDisplayCoords = [&](const auto& y, const auto& x) {
+        const unsigned dispY = (y * pxStepY) + sensorY;
+        const unsigned dispX = sensorX - (x * pxStepX);
+        return cv::Point(dispY, dispX);
+    };
 
     while (cv::waitKey(5) != 27) { // Esc key
         cap >> frame;
         assert(!frame.empty());
-        // take the closest object's data, for the sensor at index 0
-        auto detectionData = stateDB.getSensorData(0)[0];
-        //auto frac = 0.0;
-        //if (detectionData) {
-        //    auto polarRadius = detectionData->getPolarRadius();
-        //    bGraph.drawTxt(
-        //        frame, buildDisplayTextValue(frac));
-        //    frac = polarRadius / 30.0;
-        //}
-        auto frac = s1.getFraction();
-        bGraph.draw(frame, frac);
-        cv::imshow("Live", frame);
+        // get data from 1 sensor only, at index 0
+        for (const auto obstacle : stateDB.getSensorData(0)) {
+            if (obstacle) {
+                auto y = obstacle->getY();
+                auto x = obstacle->getX();
+                auto obstP = toDisplayCoords(y, x);
+
+                // draw obstacle
+                cv::circle(frame, obstP, obstRadius, obstColor,
+                           -1 /* filled circle */, cv::LINE_AA /* line type */);
+                cv::line(frame, sensorP, obstP, obstColor, 1 /* thickness */,
+                         cv::LINE_8 /* line type */);
+            }
+        }
+        cv::rectangle(frame, rectP1, rectP2, rectColor);
+        cv::imshow("Augmented Reality App", frame);
     }
 }
 
@@ -83,23 +120,23 @@ int main(int argc, char** argv)
     try {
         static constexpr unsigned N_SENSORS = 1;
 
-        //can::CANproChannel channel;
+        can::CANproChannel channel;
         can::backsense::RadarStateDB stateDB(N_SENSORS);
 
         // start a task to handle the CAN bus and DB updates
-        //std::promise<void> exitSignal;
-        //std::future<void> futureSignal = exitSignal.get_future();
-        //std::thread canHandler(can::CANUtils::interruption, channel.getHandle(),
-        //                       std::ref(stateDB), std::move(futureSignal));
+        std::promise<void> exitSignal;
+        std::future<void> futureSignal = exitSignal.get_future();
+        std::thread canHandler(can::CANUtils::interruption, channel.getHandle(),
+                               std::ref(stateDB), std::move(futureSignal));
 
         // blocking call: loop until the user quits
         launchARWindowLoop(stateDB);
 
         // notify interruption thread
-        //exitSignal.set_value();
+        exitSignal.set_value();
 
-        //can::CANUtils::resetChip(channel.getHandle());
-        //canHandler.join();
+        can::CANUtils::resetChip(channel.getHandle());
+        canHandler.join();
 
     } catch (std::runtime_error& ex) {
         std::cerr << "#ERROR: " << ex.what() << std::endl;
